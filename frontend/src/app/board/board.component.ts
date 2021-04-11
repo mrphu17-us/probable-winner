ee496d114b1506bfe2c0a810323d107d8bae6b1a
John <mrphu17@gmail.com>
2021-04-10 22:18:31 -0500

    add new theme

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { NewCardComponent } from './new-card.component';
+import { UpdateCardComponent } from './update-card.component';
 @Component({
   selector: 'app-board',
   templateUrl: "./board.component.html",
@@ -10,17 +11,37 @@ import { NewCardComponent } from './new-card.component';
     }
 
     button { float: right; }
+
+    .box {
+      overflow: hidden; color: black; 
+      border-radius: 0.25rem; 
+      border: 0.0625rem solid #e4e9f2; 
+      padding: 5px; width: 100%;
+      background-color: #e4e9f2;
+    }
   `]
 })
 
 export class BoardComponent implements OnInit {
   constructor(private dialogService: NbDialogService) { }
-  public fruits: any = ['a', 'b'];
+  public fruits: any = [{title: "this is title", name: "Phu"}, 
+  {title: "this is title", name: "Sto"},
+  {title: "this is title", name: "Sto"},
+  {title: "this is title", name: "Sto"},
+  {title: "this is title", name: "Sto"}];
   ngOnInit(): void {
   }
 
   addNewCard(type: string) {
     this.dialogService.open(NewCardComponent, {
+      context: {
+        // title: 'This is a title passed to the dialog component',
+      },
+    });
+  }
+
+  updateCard(id: string) {
+    this.dialogService.open(UpdateCardComponent, {
       context: {
         // title: 'This is a title passed to the dialog component',
       },
