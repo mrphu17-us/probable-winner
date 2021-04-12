e9144d13b8de988ccaafef21fc649bc01af1c1e7
John <mrphu17@gmail.com>
2021-04-12 18:10:51 -0500

    redirect

--- frontend/src/app/page-routing.module.ts
+++ frontend/src/app/page-routing.module.ts
@@ -38,6 +38,11 @@ const routes: Routes = [
     path: 'projects/list',
     component: ProjectListComponent,
   },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'projects/list',
+  },
 ];
 
 @NgModule({
