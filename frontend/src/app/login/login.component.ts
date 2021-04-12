3c94686890e9852e7dd600840cf3f62510bf3885
Stoyan Terziev <stole.t@outlook.com>
2021-04-12 12:46:00 -0500

    style added back

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls:['./login.component.css']
 })
 export class LoginComponent  implements OnInit {
   myForm:FormGroup;
